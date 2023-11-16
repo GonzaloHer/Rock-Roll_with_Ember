8290aa7702df533be42529d66c17667771a96b06
gonzalo hernandez <gonzalo.hernandez@mimiquate.com>
2023-11-16 14:44:48 -0300

    until page 180

--- app/models/band.js
+++ app/models/band.js
@@ -1,12 +1,17 @@
 import { tracked } from '@glimmer/tracking';
+
+const defaultRelationships = {
+  songs: 'songs',
+};
 export default class Band {
   @tracked name;
   @tracked songs;
 
-  constructor({ id, name, songs }, relationships = {}) {
+  constructor({ id, name, songs, description }, relationships) {
     this.id = id;
     this.name = name;
     this.songs = songs || [];
-    this.relationships = relationships;
+    this.relationships = relationships || defaultRelationships;
+    this.description = description;
   }
 }
